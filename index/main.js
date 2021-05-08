f9756d8613e5d349db531bb7cd358c114ebafb84
Alex Aguilar <mramireza1700alumno.ipn.mx>
2021-05-08 00:32:29 -0500

    Se modificó el main.js para analizar la API en consola

--- index/main.js
+++ index/main.js
@@ -1,1 +1,8 @@
-const 
+const api = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY'
+
+fetch(api)
+.then( response => response.json() )
+.then( data => {
+	console.log(data)
+})
+.catch( error => console.log(error) )
