a96c6019de61631b284b20b1e8df5e27ece84480
Alex Aguilar <mramireza1700alumno.ipn.mx>
2021-05-09 00:06:01 -0500

    Se modificó el main.js y el index para mostrar en pantalla

--- index/main.js
+++ index/main.js
@@ -3,6 +3,10 @@ const api = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_dat
 fetch(api)
 .then( response => response.json() )
 .then( data => {
+
+	let elem = document.getElementById('element')
+	elem.innerHTML = `<p>${data.name}</p>`
+
 	console.log(data)
 })
 .catch( error => console.log(error) )
